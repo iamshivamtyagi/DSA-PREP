27a03d6f7578d9c81294750a590fcc8918c2d803
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-03-13 16:53:31 +0530

    initial

---
+++ leetcode/src/StringReverse.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+class ReverseString {
+	public void reverseString(char[] s) {
+		int l = 0;
+		int r = s.length - 1;
+		while (l <= r) {
+			char c = s[l];
+			s[l] = s[r];
+			s[r] = c;
+			++l;
+			--r;
+		}
+	}
+}
