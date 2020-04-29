3598f61056169e1962a8f115de9076352e2096f9
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-04-29 07:21:38 +0530

    Create ListNode.java

---
+++ leetcode/src/linked_list/ListNode.java
@@ -0,0 +1,23 @@
+package linked_list;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+	
+	public void insert(int val) {
+		ListNode l = new ListNode(val);
+		
+	}	
+}
