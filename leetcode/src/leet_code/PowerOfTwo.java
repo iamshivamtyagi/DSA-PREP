edd9a8215491d9654499995b6ab6840e44532511
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-06-10 19:26:35 +0530

    PowerOfTwo

---
+++ leetcode/src/leet_code/PowerOfTwo.java
@@ -0,0 +1,42 @@
+package leet_code;
+
+public class PowerOfTwo {
+
+	/*
+	 * Problem ID : 231 (Easy)
+	 * 
+	 * Given an integer, write a function to determine if it is a power of two.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: 1 ; Output: true
+	 * 
+	 * Explanation: 2^0 = 1
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: 16 ; Output: true
+	 * 
+	 * Explanation: 2^4 = 16
+	 * 
+	 * Example 3:
+	 * 
+	 * Input: 218 ; Output: false
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println(powerOfTwo(1026));
+	}
+
+	public static boolean powerOfTwo(int n) {
+
+		long i = 1;
+
+		while (i <= n) {
+			i *= 2;
+		}
+
+		return n == i ? true : false;
+	}
+}
