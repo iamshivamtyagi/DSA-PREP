eb3f5ac6ac8d6b8e332022c2e577fd3f80240887
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-06-10 20:11:17 +0530

    SearchInsertPosition

---
+++ leetcode/src/leet_code/SearchInsertPosition.java
@@ -0,0 +1,61 @@
+package leet_code;
+
+public class SearchInsertPosition {
+
+	/*
+	 * Problem ID : 35 (Easy)
+	 * 
+	 * Given a sorted array and a target value, return the index if the target is
+	 * found. If not, return the index where it would be if it were inserted in
+	 * order.
+	 * 
+	 * You may assume no duplicates in the array.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: [1,3,5,6], 5 Output: 2
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: [1,3,5,6], 2 Output: 1
+	 * 
+	 * Example 3:
+	 * 
+	 * Input: [1,3,5,6], 7 Output: 4
+	 * 
+	 * Example 4:
+	 * 
+	 * Input: [1,3,5,6], 0 Output: 0
+	 */
+
+	public static void main(String[] args) {
+		int[] a = new int[] { 1, 3, 5, 6 };
+		System.out.println(searchInsert(a, 0));
+	}
+
+	public static int searchInsert(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+
+		if (nums[right] < target) {
+			return right + 1;
+		}
+		if (nums[left] > target) {
+			return left;
+		}
+
+		int mid = 0;
+
+		while (left <= right) {
+			mid = (left - right) / 2 + right;
+			if (nums[mid] == target)
+				return mid;
+			else if (target < nums[mid]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return left;
+	}
+}
