a0ca1309d87e7ef4989a28ae44a1dc3ce500460c
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-07-09 21:03:44 +0530

    add one to number

--- leetcode/src/ib_arrays/AddOneToNumber.java
+++ leetcode/src/ib_arrays/AddOneToNumber.java
@@ -37,72 +37,95 @@ public class AddOneToNumber {
 	 * A : For the purpose of this question, NO. Even if the input has zeroes before
 	 * the most significant digit.
 	 */
-	public static void main(String[] args) {
 
-		int[] a = plusOne(new int[] { 0, 1, 2, 3, 9 });
+	public static void main(String[] args) {
+		int[] a = plusOne(new int[] { 1, 9, 9, 9, 9, 9 });
 
 		for (int i : a)
 			System.out.print(i + " ");
 
-		System.out.println();
 	}
 
-	private static int[] plusOne(int[] a) {
+	public static int[] plusOne(int[] A) {
+		// size of original array
+		int size = A.length;
 
-		int size = a.length;
+		/*
+		 * to store intermediate result
+		 *
+		 * we store the element of original array from right to left by adding carry in
+		 * this array.
+		 * 
+		 */
+		int[] arr = new int[size];
+
+		// carry will be one initially
+		int carry = 1;
+
+		/*
+		 * Filling the intermediate array :
+		 * 
+		 * 1) We add the carry to original array's last element & store it in our
+		 * intermediate array.
+		 * 
+		 * 2) We calculate new carry by dividing recently updated element by 10. If
+		 * after adding carry number become >= 10 , carry updated to 1 else remain 0.
+		 * 
+		 * 3) Since, element at an index represent a single digit of number, we modulus
+		 * current number by 10.
+		 */
+		for (int i = size - 1; i >= 0; i--) {
+			arr[i] = A[i] + carry;
+			carry = arr[i] / 10;
+			arr[i] = arr[i] % 10;
+		}
+
 		int count = 0;
-		boolean isNeg = false;
 
-		if (size >= 1)
-			if (a[0] < 0)
-				isNeg = true;
-
-		for (int i = 0; i < size; i++) {
-			if (a[i] == 0)
-				count++;
-			else
-				break;
-		}
-
-		reverseArray(a);
-
-		int j = 0;
-		int carry = isNeg ? -1 : 1;
-
-		for (int i = 0; i < size - count; i++) {
-
-			j = a[i] + carry;
-
-			carry = j / 10;
-
-			a[i] = j % 10;
-		}
-
-		if (carry != 0) {
-			int[] b = new int[size + 1];
-			b[0] = carry;
-
-			for (int i = 1; i < b.length; i++) {
-				b[i] = a[i - 1];
+		/*
+		 * if carry is zero, we count leading zeroes in our intermediate array if
+		 * present
+		 */
+		if (carry == 0) {
+			for (int i : arr) {
+				if (i != 0)
+					break;
+				++count;
 			}
 		}
+		/*
+		 * if carry is 1, then all remaining numbers are definitely 0.
+		 *
+		 * we simply create a new array with size greater than 1 from intermediate array
+		 * & insert 1 at the beginning of it.
+		 * 
+		 */
 
-		reverseArray(a);
-
-		return a;
-	}
-
-	private static void reverseArray(int[] a) {
-		int i = 0;
-		int j = a.length - 1;
-
-		while (i <= j) {
-			int t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-			++i;
-			--j;
+		else {
+			int[] ans = new int[size + 1];
+			ans[0] = 1;
+			return ans;
 		}
+
+		// if no leading zeroes, then intermediate array is the answer
+		if (count == 0) {
+			return arr;
+		}
+
+		// if count is more than 0, we reduce size by zero coun & fill final array from
+		// left to right
+		int[] ans = new int[size - count];
+
+		int l = ans.length - 1;
+		int r = arr.length - 1;
+
+		while (l >= 0 && r >= count) {
+			ans[l] = arr[r];
+			--l;
+			--r;
+		}
+
+		return ans;
 	}
 
 }
