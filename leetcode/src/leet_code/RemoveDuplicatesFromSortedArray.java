e40e7524c6bb0199a893a635ffea3a0e10d0da77
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-08-14 23:23:34 +0530

    remove dup

--- leetcode/src/leet_code/RemoveDuplicatesFromSortedArray.java
+++ leetcode/src/leet_code/RemoveDuplicatesFromSortedArray.java
@@ -33,7 +33,7 @@ public class RemoveDuplicatesFromSortedArray {
 
 	public static void main(String[] args) {
 
-		int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 2, 3};
 
 		int len = removeDuplicatesFromSortedArray(arr);
 
